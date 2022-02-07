b57d8e1f7ea06914c2d6119db8c46b0d50c837fd
VladCirnik <depuregercplay@gmail.com>
2022-02-07 14:48:40 +0500

    auto iris farm ic

---
+++ vkplus/commands/auto_iris_farm.py
@@ -0,0 +1,22 @@
+from vkbottle.user import Blueprint
+import time
+import asyncio
+
+bp = Blueprint("AutoIrisFarm")
+
+
+async def main():
+    while True:
+        comment_id = await bp.api.wall.create_comment(
+            owner_id=-174105461,
+            post_id=6713149,
+            message="ферма"
+            )
+        await bp.api.wall.delete_comment(
+            owner_id=-174105461,
+            comment_id=comment_id
+        )
+        time.sleep(14400)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
